7e7f7fd1825e1ece047d63defed7fdc00528480d
Maria <maria.mihaila.17@gmail.com>
2023-09-21 22:04:01 +0300

    remove cache and add dist to gitignore

--- server/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-const app = () => {
-    console.log('aloha');
-};
-exports.app = app;
-(0, exports.app)();
-//# sourceMappingURL=index.js.map
