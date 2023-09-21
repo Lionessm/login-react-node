9323ca962128b2788c234e7341e0bdb1a7eb44ba
Maria <maria.mihaila.17@gmail.com>
2023-09-21 21:59:08 +0300

    validate setup

---
+++ server/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const app = () => {
+    console.log('aloha');
+};
+exports.app = app;
+(0, exports.app)();
+//# sourceMappingURL=index.js.map
